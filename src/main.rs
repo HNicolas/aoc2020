c00a30cb05aa05124eb5c4ec648ac6d8fe2221c7
Nicolas Hervé <nicolas.herve.pessac@gmail.com>
2020-12-09 23:44:28 +0100

    Start working on day 4 solution

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod day1;
 mod day2;
 mod day3;
+mod day4;
 mod day7;
 mod day8;
 
@@ -8,6 +9,7 @@ fn main() {
     day1::run();
     day2::run();
     day3::run();
+    day4::run();
     day7::run();
     day8::run();
 }
