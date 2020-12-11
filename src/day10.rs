3167814b189592aed49ff0d3ea60b3d69c8d3b23
Nicolas Hervé <nicolas.herve.pessac@gmail.com>
2020-12-11 11:49:04 +0100

    Update solution for day 10

--- src/day10.rs
+++ src/day10.rs
@@ -1,26 +1,12 @@
-use std::collections::HashMap;
-
-fn solve_1(values: &Vec<u8>) -> u32 {
-    let mut current_jolt = 0;
+fn solve_1(sorted_values: &Vec<u8>) -> u32 {
     let mut diff_1 = 0;
     let mut diff_3 = 0;
-
-    loop {
-        if values.contains(&(current_jolt + 1)) {
-            current_jolt += 1;
-            diff_1 += 1;
-            continue;
+    for i in 0..(sorted_values.len() - 1) {
+        match sorted_values[i + 1] - sorted_values[i] {
+            1 => diff_1 += 1,
+            3 => diff_3 += 1,
+            _ => {}
         }
-        if values.contains(&(current_jolt + 2)) {
-            current_jolt += 2;
-            continue;
-        }
-        if values.contains(&(current_jolt + 3)) {
-            current_jolt += 3;
-            diff_3 += 1;
-            continue;
-        }
-        break;
     }
     diff_1 * (diff_3 + 1)
 }
@@ -39,38 +25,31 @@ Avoid recursion
 16 -> from 15 -> 8
 19 -> from 16 -> 8
 */
-fn solve_2(values: &mut Vec<u8>) -> u64 {
-    values.sort();
-
-    let mut map: HashMap<u8, u64> = HashMap::new();
-    map.insert(0, 1);
-
-    for &value in values.iter() {
-        let mut paths = 0;
-        if value > 0 && map.contains_key(&(value - 1)) {
-            paths += map.get(&(value - 1)).unwrap();
+fn solve_2(sorted_values: &Vec<u8>) -> u64 {
+    let mut paths = vec![0; sorted_values.len()];
+    paths[0] = 1;
+    for i in 0..sorted_values.len() - 1 {
+        let mut j = i + 1;
+        while j < sorted_values.len() && sorted_values[j] - sorted_values[i] <= 3 {
+            paths[j] += paths[i];
+            j += 1;
         }
-        if value > 1 && map.contains_key(&(value - 2)) {
-            paths += map.get(&(value - 2)).unwrap();
-        }
-        if value > 2 && map.contains_key(&(value - 3)) {
-            paths += map.get(&(value - 3)).unwrap();
-        }
-        map.insert(value, paths);
     }
-
-    map.get(&values[values.len() - 1]).unwrap().to_owned()
+    paths[paths.len() - 1]
 }
 
 fn parse_input(input: &str) -> Vec<u8> {
-    input.lines().map(|s| s.parse().unwrap()).collect()
+    let mut vec: Vec<u8> = input.lines().map(|s| s.parse().unwrap()).collect();
+    vec.push(0);
+    vec.sort();
+    vec
 }
 
 pub fn run() {
     let input = std::fs::read_to_string("inputs/day10").unwrap();
-    let mut values = parse_input(&input);
-    println!("day 10 solution 1 : {}", solve_1(&values));
-    println!("day 10 solution 2 : {}", solve_2(&mut values));
+    let sorted_values = parse_input(&input);
+    println!("day 10 solution 1 : {}", solve_1(&sorted_values));
+    println!("day 10 solution 2 : {}", solve_2(&sorted_values));
 }
 
 #[cfg(test)]
