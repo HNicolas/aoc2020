530d076b8ae2ff47bbe0f7621a414123506e7705
Nicolas Hervé <nicolas.herve.pessac@gmail.com>
2020-12-17 19:17:31 +0100

    Update solution for day 17 to n dim

--- src/day17.rs
+++ src/day17.rs
@@ -1,39 +1,83 @@
 use std::collections::{HashMap, HashSet};
 
-#[derive(PartialEq, Eq, Hash, Clone, Copy)]
-struct Coordinates(i16, i16, i16);
+struct CellNeighbours<'a> {
+    cell: &'a Vec<i16>,
+    index: u8,
+    total: u8,
+}
 
-impl Coordinates {
-    fn get_neighboors(&self) -> Vec<Self> {
-        (self.0 - 1..=self.0 + 1)
-            .flat_map(|x| {
-                (self.1 - 1..=self.1 + 1).flat_map(move |y| {
-                    (self.2 - 1..=self.2 + 1).filter_map(move |z| {
-                        if Self(x, y, z) == *self {
-                            None
-                        } else {
-                            Some(Self(x, y, z))
-                        }
-                    })
-                })
-            })
-            .collect::<Vec<_>>()
+impl<'a> CellNeighbours<'a> {
+    fn new(cell: &'a Vec<i16>) -> Self {
+        Self {
+            cell,
+            index: 0,
+            total: 3u8.pow(cell.len() as u32),
+        }
+    }
+}
+
+impl<'a> Iterator for CellNeighbours<'a> {
+    type Item = Vec<i16>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.index == (self.total - 1) / 2 {
+            self.index += 1;
+        }
+
+        if self.index == self.total {
+            None
+        } else {
+            let sum_vec = to_base(self.index, 3);
+            let result = self
+                .cell
+                .iter()
+                .enumerate()
+                .fold(vec![], |mut vec, (index, value)| {
+                    vec.push(
+                        value - 1
+                            + if index < sum_vec.len() {
+                                sum_vec[index] as i16
+                            } else {
+                                0
+                            },
+                    );
+                    vec
+                });
+            self.index += 1;
+            Some(result)
+        }
+    }
+}
+
+fn to_base(number: u8, base: u8) -> Vec<u8> {
+    let mut result = vec![];
+    let mut quot = number;
+    loop {
+        let rest = quot % base;
+        result.push(rest);
+        quot = (quot - rest) / base;
+        if quot == 0 {
+            break result;
+        }
     }
 }
 
 struct Game {
-    active_cells: HashSet<Coordinates>,
+    active_cells: HashSet<Vec<i16>>,
 }
 
 impl Game {
-    fn new(input: &str) -> Self {
+    fn new(input: &str, dimension: usize) -> Self {
         let active_cells = input
             .lines()
             .enumerate()
             .flat_map(|(x, line)| {
                 line.chars().enumerate().filter_map(move |(y, c)| {
                     if c == '#' {
-                        Some(Coordinates(x as i16, y as i16, 0))
+                        let mut vec = vec![0; dimension];
+                        vec[0] = x as i16;
+                        vec[1] = y as i16;
+                        Some(vec)
                     } else {
                         None
                     }
@@ -45,89 +89,9 @@ impl Game {
     }
 
     fn next_state(&mut self) {
-        let mut active_neighbours_map = HashMap::<Coordinates, u8>::new();
+        let mut active_neighbours_map = HashMap::<Vec<i16>, u8>::new();
         for active_cell in &self.active_cells {
-            for neighbour_cell in active_cell.get_neighboors() {
-                if let Some(active_neighbours) = active_neighbours_map.get_mut(&neighbour_cell) {
-                    *active_neighbours += 1;
-                } else {
-                    active_neighbours_map.insert(neighbour_cell, 1);
-                }
-            }
-        }
-
-        let mut new_active_cells = HashSet::new();
-        for (cell, active_neighboors) in active_neighbours_map {
-            match active_neighboors {
-                3 => {
-                    new_active_cells.insert(cell);
-                }
-                2 if self.active_cells.contains(&cell) => {
-                    new_active_cells.insert(cell);
-                }
-                _ => {}
-            }
-        }
-        self.active_cells = new_active_cells;
-    }
-
-    fn compute_cycles(&mut self, number: usize) {
-        for _ in 0..number {
-            self.next_state();
-        }
-    }
-}
-
-#[derive(PartialEq, Eq, Hash, Clone, Copy)]
-struct Coordinates4d(i16, i16, i16, i16);
-
-impl Coordinates4d {
-    fn get_neighboors(&self) -> Vec<Self> {
-        (self.0 - 1..=self.0 + 1)
-            .flat_map(|x| {
-                (self.1 - 1..=self.1 + 1).flat_map(move |y| {
-                    (self.2 - 1..=self.2 + 1).flat_map(move |z| {
-                        (self.3 - 1..=self.3 + 1).filter_map(move |w| {
-                            if Self(x, y, z, w) == *self {
-                                None
-                            } else {
-                                Some(Self(x, y, z, w))
-                            }
-                        })
-                    })
-                })
-            })
-            .collect::<Vec<_>>()
-    }
-}
-
-struct Game4d {
-    active_cells: HashSet<Coordinates4d>,
-}
-
-impl Game4d {
-    fn new(input: &str) -> Self {
-        let active_cells = input
-            .lines()
-            .enumerate()
-            .flat_map(|(x, line)| {
-                line.chars().enumerate().filter_map(move |(y, c)| {
-                    if c == '#' {
-                        Some(Coordinates4d(x as i16, y as i16, 0, 0))
-                    } else {
-                        None
-                    }
-                })
-            })
-            .collect();
-
-        Self { active_cells }
-    }
-
-    fn next_state(&mut self) {
-        let mut active_neighbours_map = HashMap::<Coordinates4d, u8>::new();
-        for active_cell in &self.active_cells {
-            for neighbour_cell in active_cell.get_neighboors() {
+            for neighbour_cell in CellNeighbours::new(active_cell) {
                 if let Some(active_neighbours) = active_neighbours_map.get_mut(&neighbour_cell) {
                     *active_neighbours += 1;
                 } else {
@@ -161,7 +125,7 @@ impl Game4d {
 pub fn run() {
     let timer = std::time::Instant::now();
     let input = std::fs::read_to_string("inputs/day17").unwrap();
-    let mut game = Game::new(&input);
+    let mut game = Game::new(&input, 3);
     game.compute_cycles(6);
     println!(
         "day 17 solution 1 : {}, {}us",
@@ -169,7 +133,7 @@ pub fn run() {
         timer.elapsed().as_micros()
     );
 
-    let mut game = Game4d::new(&input);
+    let mut game = Game::new(&input, 4);
     game.compute_cycles(6);
     println!(
         "day 17 solution 2 : {}, {}us",
@@ -185,7 +149,7 @@ mod tests {
     #[test]
     fn test_solution_1() {
         let input = ".#.\n..#\n###";
-        let mut game = Game::new(&input);
+        let mut game = Game::new(&input, 3);
         game.compute_cycles(6);
         assert_eq!(game.active_cells.len(), 112);
     }
@@ -193,7 +157,7 @@ mod tests {
     #[test]
     fn test_solution_2() {
         let input = ".#.\n..#\n###";
-        let mut game = Game4d::new(&input);
+        let mut game = Game::new(&input, 4);
         game.compute_cycles(6);
         assert_eq!(game.active_cells.len(), 848);
     }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,4 @@
 mod day1;
-mod day2;
-mod day3;
-mod day4;
-mod day5;
-mod day6;
-mod day7;
-mod day8;
-mod day9;
 mod day10;
 mod day11;
 mod day12;
@@ -15,6 +7,14 @@ mod day14;
 mod day15;
 mod day16;
 mod day17;
+mod day2;
+mod day3;
+mod day4;
+mod day5;
+mod day6;
+mod day7;
+mod day8;
+mod day9;
 
 fn main() {
     let timer = std::time::Instant::now();
